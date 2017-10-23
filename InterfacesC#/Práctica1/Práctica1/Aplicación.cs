92cbf6b375396135c1a2c72f5da34d8ea2f6e3f5
CristinaNavarro <cris9.7@hotmail.com>
2017-10-23 21:37:40 +0200

    Añadida práctica de interfaces (C#), funcionamiento de un televisor.

---
+++ InterfacesC#/Práctica1/Práctica1/Aplicación.cs
@@ -0,0 +1,248 @@
+﻿/* 
+ * * PRÁCTICA.............: Práctica 1. 
+ * * NOMBRE y APELLIDOS...: Cristina Navarro Soto
+ * * CURSO y GRUPO........: 2º Desarrollo de Interfaces 
+ * * TÍTULO de la PRÁCTICA: Diseño de clases. Herencia y polimorfismo. 
+ * * FECHA de ENTREGA.....: 23 de octubre de 2017 
+ */
+
+using System;
+
+namespace Práctica1 {
+
+    class Aplicación {
+
+        static void Main(string[] args) {
+            Tv televisor = comprarTelevisor();
+            ConsoleKeyInfo tecla;
+            bool boValido;
+            byte byCambioCanal = 0;
+
+            do {
+                imprimirMenu(televisor);
+                tecla = Console.ReadKey();
+
+                switch (tecla.Key) {
+                    case ConsoleKey.P:
+                        televisor.pulsarEncendido();
+                        imprimirMenu(televisor);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        televisor.subirVolumen();
+                        imprimirMenu(televisor);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        televisor.bajarVolumen();
+                        imprimirMenu(televisor);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        do {
+                            imprimirMenu(televisor);
+                            boValido = false;
+
+                            if (televisor.getBoEncendido()) {
+                                Console.Write("\n\nIntroduzca un canal entre 0 y 99 y confírmelo pulsando ENTER: ");
+                                string strCadena = Console.ReadLine();
+                                boValido = esNumeroValido(strCadena, "Byte");
+
+                                if (boValido) {
+                                    byCambioCanal = Convert.ToByte(strCadena);
+
+                                    if (televisor.getBoEncendido() && byCambioCanal < 100) {
+                                        televisor.ponerCanal(byCambioCanal);
+                                    } else {
+                                        esNumeroValido("-1", "Nopermitido");
+                                        boValido = false;
+                                    }
+                                }
+                            } else {
+                                Console.Beep();
+                                Console.Beep();
+                                alertaExcepcion("Por favor, encienda el televisor para modificar el volumen o el canal.");
+                                Console.Clear();
+                                boValido = true;
+                            }
+
+                        } while (!boValido);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        televisor.cambiarCanalAnterior();
+                        imprimirMenu(televisor);
+                        break;
+                    case ConsoleKey.I:
+                        televisor.informacionTecnica();
+                        imprimirMenu(televisor);
+                        break;
+                    case ConsoleKey.Escape:
+                        break;
+                    default:
+                        break;
+                }
+            } while (tecla.Key != ConsoleKey.Escape);
+        }
+
+        private static Tv comprarTelevisor() {
+            string strMarca;
+            float floPulgadas = 0;
+            float floConsumo = 0;
+            float floPrecio = 0;
+            bool boValido = false;
+            string strCadena;
+
+            do {
+                Console.Clear();
+                Console.Write("Introduzca la marca de su nuevo televisor: \n\t->");
+                strMarca = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(strMarca)) {
+
+                    alertaExcepcion("Es necesario introducir una marca de televisión.");
+
+                    Console.Clear();
+                }
+            } while (String.IsNullOrWhiteSpace(strMarca));
+
+            do {
+                boValido = false;
+                Console.Write("\nIntroduzca las pulgadas: \n\t->");
+                strCadena = Console.ReadLine();
+                boValido = esNumeroValido(strCadena, "Single");
+
+                if (boValido) {
+                    floPulgadas = Convert.ToSingle(strCadena);
+
+                    if (floPulgadas > 400 && floPulgadas < 4) {
+                        alertaExcepcion("Solo se venden televisores de entre 4 y 400 pulgadas.");
+                        boValido = false;
+                        Console.Clear();
+                        Console.Write("La marca de su televisor es: " + strMarca + "\n");
+                    } else if (floPulgadas <= 0) {
+                        alertaExcepcion("No se puede introducir un número negativo o 0.");
+                        boValido = false;
+                        Console.Clear();
+                        Console.Write("La marca de su televisor es: " + strMarca + "\n");
+                    }
+
+                } else {
+                    Console.Clear();
+                    Console.Write("La marca de su televisor es: " + strMarca + "\n");
+                }
+
+            } while (!boValido);
+
+            do {
+                boValido = false;
+                Console.Write("\nIntroduzca el consumo energético: \n\t->");
+                strCadena = Console.ReadLine();
+                boValido = esNumeroValido(strCadena, "Single");
+
+                if (boValido) {
+                    floConsumo = Convert.ToSingle(strCadena);
+
+                    if (floConsumo <= 0) {
+                        boValido = false;
+                        alertaExcepcion("No se puede introducir un consumo negativo o 0.");
+                        Console.Clear();
+                        Console.Write("La marca de su televisor es: " + strMarca + "\n");
+                        Console.Write("\nSu televisor es de " + floPulgadas + " pulgadas\n");
+                    }
+
+                } else {
+                    Console.Clear();
+                    Console.Write("La marca de su televisor es: " + strMarca + "\n");
+                    Console.Write("\nSu televisor es de " + floPulgadas + " pulgadas\n");
+                }
+
+            } while (!boValido);
+
+            do {
+                boValido = false;
+                Console.Write("\nIntroduzca el precio: \n\t->");
+                strCadena = Console.ReadLine();
+                boValido = esNumeroValido(strCadena, "Single");
+
+                if (boValido) {
+                    floPrecio = Convert.ToSingle(strCadena);
+
+                    if (floPrecio < 0) {
+                        alertaExcepcion("No se puede introducir un precio negativo."); //Se permite un televisor regalado (floPrecio = 0)
+                        boValido = false;
+                        Console.Clear();
+                        Console.Write("La marca de su televisor es: " + strMarca);
+                        Console.Write("\n\nSu televisor es de " + floPulgadas + " pulgadas");
+                        Console.Write("\n\nSu televisor consume " + floConsumo + "W\n");
+                    }
+
+                } else {
+                    Console.Clear();
+                    Console.Write("La marca de su televisor es: " + strMarca);
+                    Console.Write("\n\nSu televisor es de " + floPulgadas + " pulgadas");
+                    Console.Write("\n\nSu televisor consume " + floConsumo + "W\n");
+                }
+
+            } while (!boValido);
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.Write("\n\nHa comprado un televisor de la marca " + strMarca + ".");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(2000);
+            return new Tv(strMarca, floPulgadas, floConsumo, floPrecio);
+        }
+
+        private static void imprimirMenu(Tv televisor) {
+            Console.Clear();
+            Console.Write("Mando de control de la televisión");
+
+            if (!televisor.getBoEncendido()) {
+                Console.Write(" para modo APAGADO: \n");
+                Console.Write("\n\t- Pulse P para encender la televisión.");
+                Console.Write("\n\t- Pulse ESCAPE para salir.");
+                Console.Write("\n\t- Pulse I para ver la información técnica de su televisor.");
+            } else {
+                Console.Write(" para modo ENCENDIDO: \n");
+                Console.Write("\n\t- Pulse P para apagar la televisión.");
+                Console.Write("\n\t- Pulse ESCAPE para salir.");
+                Console.Write("\n\t- Pulse I para ver la información técnica de su televisor.");
+                Console.Write("\n\t- Pulse ↑(flecha arriba) para subir el volumen.");
+                Console.Write("\n\t- Pulse ↓(flecha abajo) para bajar el volumen.");
+                Console.Write("\n\t- Pulse →(flecha derecha) para introducir un canal.");
+                Console.Write("\n\t- Pulse ←(flecha izquierda) para volver al canal anterior.");
+                televisor.imprimirDatosEncendido();
+            }
+        }
+
+        static bool esNumeroValido(string strNumero, string strTipo) {
+            bool boValido = true;
+
+            if (strNumero.Contains(".") && strTipo.Equals("Single")) {
+                alertaExcepcion("Utilice comas para indicar los decimales.");
+                return false;
+            }
+
+            try {
+
+                if (strTipo.Equals("Single")) {
+                    float floComprobar = Convert.ToSingle(strNumero);
+                } else {
+                    byte floComprobar = Convert.ToByte(strNumero);
+                }
+
+            } catch (OverflowException) {
+                alertaExcepcion("Ha introducido un número demasiado grande o negativo.");
+                boValido = false;
+            } catch (FormatException) {
+                alertaExcepcion("No ha introducido un número.");
+                boValido = false;
+            }
+
+            return boValido;
+        }
+
+        private static void alertaExcepcion(String strMensaje) {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("\n" + strMensaje);
+            Console.ResetColor();
+            Console.Beep();
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
---
+++ InterfacesC#/Práctica1/Práctica1/Tv.cs
@@ -0,0 +1,155 @@
+﻿/* 
+ * * PRÁCTICA.............: Práctica 1. 
+ * * NOMBRE y APELLIDOS...: Cristina Navarro Soto
+ * * CURSO y GRUPO........: 2º Desarrollo de Interfaces 
+ * * TÍTULO de la PRÁCTICA: Diseño de clases. Herencia y polimorfismo. 
+ * * FECHA de ENTREGA.....: 23 de octubre de 2017 
+ */
+
+using System;
+
+
+namespace Práctica1 {
+    class Tv {
+        #region Atributos
+        string strMarca;
+        float floPulgadas;
+        float floConsumo;
+        float floPrecio;
+        bool boEncendido;
+        byte byCanal; //0-99
+        byte byCanalAnterior;
+        byte byVolumen; //1-100
+        #endregion
+
+        #region Constructor
+        public Tv(string strMarca, float floPulgadas, float floConsumo, float floPrecio) {
+            this.strMarca = strMarca;
+            this.floPulgadas = floPulgadas;
+            this.floConsumo = floConsumo;
+            this.floPrecio = floPrecio;
+            this.boEncendido = false;
+            iniciarPorDefecto();
+        }
+        #endregion
+
+        #region Getter&Setter
+        public bool getBoEncendido() {
+            return this.boEncendido;
+        }
+        #endregion
+
+        #region Métodos
+        private void iniciarPorDefecto() {
+            this.byCanal = 0;
+            this.byCanalAnterior = 0;
+            this.byVolumen = 0;
+        }
+
+        public void pulsarEncendido() {
+            if (boEncendido) {
+                this.boEncendido = false;
+                this.iniciarPorDefecto();
+            } else {
+                this.boEncendido = true;
+                this.byCanal = 1;
+                this.byCanalAnterior = byCanal;
+                this.byVolumen = 25;
+            }
+        }
+
+        public void subirVolumen() {
+            if (this.boEncendido) {
+                if (this.byVolumen != 100) {
+                    this.byVolumen++;
+                    Console.Write(this.byVolumen);
+                } else {
+                    Console.Beep();
+                    alertaDatoIncorrecto("No se puede aumentar el volumen por encima de 100.");
+                }
+            } else {
+                alertaTelevisorApagado();
+            }
+        }
+
+        public void bajarVolumen() {
+            if (this.boEncendido) {
+                if (this.byVolumen > 0) {
+                    this.byVolumen--;
+                    Console.Write(this.byVolumen);
+                } else {
+                    Console.Beep();
+                    alertaDatoIncorrecto("No se puede disminuir el volumen por debajo de 0.");
+                }
+            } else {
+                alertaTelevisorApagado();
+            }
+        }
+
+        public void ponerCanal(byte nuevoCanal) {
+            if (this.boEncendido) {
+                this.byCanalAnterior = this.byCanal;
+                this.byCanal = nuevoCanal;
+                Console.Write("\n\nEncendida -> Canal: " + this.byCanal + ", Volumen: " + this.byVolumen);
+            } else {
+                alertaTelevisorApagado();
+            }
+
+        }
+
+        public void cambiarCanalAnterior() {
+            byte aux = 0;
+            if (this.boEncendido) {
+                aux = this.byCanal;
+                this.byCanal = this.byCanalAnterior;
+                this.byCanalAnterior = aux;
+            } else {
+                alertaTelevisorApagado();
+            }
+        }
+
+        private void estadoActual() {
+            if (this.boEncendido) {
+                Console.Write(this.byCanal);
+                Console.Write(this.byVolumen);
+            } else {
+                Console.Write("Apagado.");
+            }
+        }
+
+        public void informacionTecnica() {
+            do {
+                Console.Clear();
+                Console.Write("Información técnica de mi tele: \n\n");
+                Console.Write("\tMarca: " + this.strMarca);
+                Console.Write(String.Format("\n\tTamaño en pulgadas: {0:G3}", this.floPulgadas));
+                Console.Write(String.Format("\n\tConsumo: {0:G3} W", this.floConsumo));
+                Console.Write(String.Format("\n\tPrecio: {0:G3} euros.", this.floPrecio));
+                Console.Write("\n\tPulse ENTER para continuar...");
+            } while (Console.ReadKey().Key != ConsoleKey.Enter);
+        }
+
+        private void alertaTelevisorApagado() {
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("\nPor favor, encienda el televisor para modificar el volumen o el canal.");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(2000);
+
+        }
+
+        public void alertaDatoIncorrecto(string strInformacion) {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("\n\n" + strInformacion);
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(2000);
+        }
+
+        public void imprimirDatosEncendido() {
+            Console.Write("\n\nEncendida -> Canal: " + this.byCanal + ", Volumen: " + this.byVolumen);
+        }
+        #endregion
+    }
+}
