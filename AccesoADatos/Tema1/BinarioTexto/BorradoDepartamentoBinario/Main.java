e3d42e1e7b4b37db1d0e21c961dea49bffe3387a
CristinaNavarro <cris9.7@hotmail.com>
2017-11-01 13:46:16 +0100

    Modificación de formato.

--- AccesoADatos/Tema1/BinarioTexto/BorradoDepartamentoBinario/Main.java
+++ AccesoADatos/Tema1/BinarioTexto/BorradoDepartamentoBinario/Main.java
@@ -27,42 +27,41 @@ public class Main {
         DataOutputStream dOut = new DataOutputStream(new FileOutputStream(modificado));
         DataInputStream dInMod = new DataInputStream(new FileInputStream(modificado));
 
-
         boolean cambio = false;
         int contDepartamentos = 0;
 
-
-        try{
+        try {
             System.out.println("Introduce el numero del departamento a eliminar: ");
             int n = sc.nextInt();
             int actual;
             //mientras que queden datos en el archivo, se buscara el que coincida con el numero introducido y se borrara
-            while(true){
-                actual=dIn.readInt();
-                if(n!=actual) {
+            while (true) {
+                actual = dIn.readInt();
+                if (n != actual) {
                     contDepartamentos++;
                     dOut.writeInt(actual);
                     dOut.writeUTF(dIn.readUTF());
                     dOut.writeUTF(dIn.readUTF());
-                    System.out.println("Linea escrita en el nuevo documento: " +dInMod.readInt() +dInMod.readUTF() +dInMod.readUTF());
-                }else{
+                    System.out.println("Linea escrita en el nuevo documento: " + dInMod.readInt() + dInMod.readUTF()
+                            + dInMod.readUTF());
+                } else {
                     dIn.readUTF();
                     dIn.readUTF();
-                    cambio=true;
+                    cambio = true;
                 }
             }
-        }catch (EOFException e){
+        } catch (EOFException e) {
             dIn.close();
             dOut.close();
             dInMod.close();
-            if(!cambio){
+            if (!cambio) {
                 System.out.println("No se ha encontrado el departamento a eliminar");
-            }else{
+            } else {
                 System.out.println("Cambios efectuados");
                 f.delete();
                 modificado.renameTo(new File("C:\\Users\\Cristi\\Desktop\\A\\Departamentos.dat"));
             }
-            System.out.println("Ahora hay un total de " +contDepartamentos +" departamentos");
+            System.out.println("Ahora hay un total de " + contDepartamentos + " departamentos");
         }
     }
 }
